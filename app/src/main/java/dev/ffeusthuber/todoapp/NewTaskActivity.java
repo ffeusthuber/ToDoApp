7dc87b703ec296f4cd8b4ec83bbdc25d60033bd0
ffeusthuber <ffeusthu@gmail.com>
2023-09-02 15:44:27 +0200

    added a Datepicker for setting a tasks finish date

--- app/src/main/java/dev/ffeusthuber/todoapp/NewTaskActivity.java
+++ app/src/main/java/dev/ffeusthuber/todoapp/NewTaskActivity.java
@@ -2,36 +2,55 @@ package dev.ffeusthuber.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class NewTaskActivity extends AppCompatActivity {
 
-    private EditText edtTxtTaskName;
+    private EditText edtTxtTaskTitle;
+    private EditText edtTxtTaskFinishDate;
+    private int finishDateDay,finishDateMonth,finishDateYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
 
-        edtTxtTaskName = findViewById(R.id.edtTxtTaskName);
+        edtTxtTaskTitle = findViewById(R.id.edtTxtTaskTitle);
+        edtTxtTaskFinishDate = findViewById(R.id.edtTxtTaskFinishDate);
     }
 
 
     public void onClick(View view){
         int id = view.getId();
         if (id == R.id.btnCreateTask) {
-            saveNewTask();
+            addTaskToToDoList();
             openActivityToDoList();
         } else if (id == R.id.btnCancel) {
             openActivityToDoList();
+        } else if (id == R.id.edtTxtTaskFinishDate || id == R.id.ibtnCalendar) {
+            final Calendar calendar =Calendar.getInstance();
+            finishDateDay = calendar.get(Calendar.DATE);
+            finishDateMonth = calendar.get(Calendar.MONTH);
+            finishDateYear = calendar.get(Calendar.YEAR);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(NewTaskActivity.this, android.R.style.Theme_DeviceDefault_Dialog, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    edtTxtTaskFinishDate.setText(getString(R.string.task_display_finishdate,day,month,year));
+                }
+            },finishDateYear,finishDateMonth,finishDateDay);
+            datePickerDialog.show();
         }
     }
 
-    private void saveNewTask() {
-        Task task = new Task(edtTxtTaskName.getText().toString());
+    private void addTaskToToDoList() {
+        Task task = new Task(edtTxtTaskTitle.getText().toString());
     }
 
     private void openActivityToDoList(){
