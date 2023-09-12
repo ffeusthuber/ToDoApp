a0145778e06bf7ae732d9ab3970c471f39abc6d6
ffeusthuber <ffeusthu@gmail.com>
2023-09-12 19:48:54 +0200

    added uid to Task.java, refactored NewTaskActivity.java

--- app/src/main/java/dev/ffeusthuber/todoapp/presentation/add_edit_task/NewTaskActivity.java
+++ app/src/main/java/dev/ffeusthuber/todoapp/presentation/add_edit_task/NewTaskActivity.java
@@ -2,28 +2,33 @@ package dev.ffeusthuber.todoapp.presentation.add_edit_task;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.Calendar;
 import java.util.Date;
 
 import dev.ffeusthuber.todoapp.R;
+import dev.ffeusthuber.todoapp.data.DBConnection;
 import dev.ffeusthuber.todoapp.data.DBConnectionImpl_Firestore;
-import dev.ffeusthuber.todoapp.model.DateParser;
 import dev.ffeusthuber.todoapp.model.Task;
 import dev.ffeusthuber.todoapp.presentation.ActivityStarter;
+import dev.ffeusthuber.todoapp.util.DateParser;
 
 public class NewTaskActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
 
     private EditText edtTxtTaskTitle;
     private EditText edtTxtTaskDescription;
     private EditText edtTxtTaskCompletionDate;
     private final DateParser dateParser = new DateParser();
-    private final DBConnectionImpl_Firestore con = new DBConnectionImpl_Firestore();
+    private final DBConnection con = new DBConnectionImpl_Firestore();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +43,14 @@ public class NewTaskActivity extends AppCompatActivity {
     public void onClick(View view){
         int id = view.getId();
         if (id == R.id.btnCreateTask) {
-            addTaskToToDoList();
+            Log.d(TAG, "onClick: User clicked btnCreateTask");
+            con.saveTask(buildTaskObject());
             ActivityStarter.openActivityToDoList(NewTaskActivity.this);
         } else if (id == R.id.btnCancel) {
+            Log.d(TAG, "onClick: User clicked btnCancel");
             ActivityStarter.openActivityToDoList(NewTaskActivity.this);
         } else if (id == R.id.edtTxtTaskFinishDate || id == R.id.ibtnCalendar) {
-            final Calendar calendar =Calendar.getInstance();
+            final Calendar calendar = Calendar.getInstance();
             int finishDateDay = calendar.get(Calendar.DATE);
             int finishDateMonth = calendar.get(Calendar.MONTH);
             int finishDateYear = calendar.get(Calendar.YEAR);
@@ -57,15 +64,11 @@ public class NewTaskActivity extends AppCompatActivity {
         }
     }
 
-    private void addTaskToToDoList() {
-        Task task = buildTaskObject();
-        con.saveTask(task);
-    }
-
     private Task buildTaskObject() {
         return new Task(
                 edtTxtTaskTitle.getText().toString(),
                 edtTxtTaskDescription.getText().toString(),
+                FirebaseAuth.getInstance().getCurrentUser().getUid(),
                 false,
                 new Date(),
                 dateParser.parseStringtoDate(edtTxtTaskCompletionDate.getText().toString()),
